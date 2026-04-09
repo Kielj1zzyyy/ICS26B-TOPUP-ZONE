f082d8923979b6e9f696cbc993791d4482401537
Aaron Ezekiel M. Saguing <saguingaaronezekiel@gmail.com>
2026-04-09 23:14:25 +0800

    created a connectionDB class for db status

---
+++ ConnectionDB.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Group8_Final;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sagui
+ */
+public class ConnectionDB {
+    
+    // Database credentials
+    private static final String URL = "jdbc:mysql://localhost:3306/it26b_saguing";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database Connected Successfully!");
+        } catch (SQLException e) {
+            System.out.println("Connection Failed!");
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+}
